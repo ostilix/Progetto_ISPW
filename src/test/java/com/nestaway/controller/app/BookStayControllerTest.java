f7c07f26559a0ba87dad6f3106d99989ec19c8b7
Stefano <ostili1226@gmail.com>
2025-12-14 17:16:55 +0100

    Corrected tests

--- src/test/java/com/nestaway/controller/app/BookStayControllerTest.java
+++ src/test/java/com/nestaway/controller/app/BookStayControllerTest.java
@@ -24,14 +24,14 @@ class BookStayControllerTest {
     @Test
     void testFindStays() {
         assertDoesNotThrow(() -> {
-            List<StayBean> stays = controller.findStays("Rome", LocalDate.of(2025, 8, 5), LocalDate.of(2025, 8, 6), 2);
+            List<StayBean> stays = controller.findStays("Rome", LocalDate.of(2026, 9, 10), LocalDate.of(2026, 9, 11), 2);
             assertFalse(stays.isEmpty(), "Should return some stays for Rome");
         });
     }
 
     @Test
     void testFindAvailability() throws Exception {
-        List<StayBean> stays = controller.findStays("Milan", LocalDate.of(2025, 8, 5), LocalDate.of(2025, 8, 6), 2);
+        List<StayBean> stays = controller.findStays("Milan", LocalDate.of(2026, 9, 10), LocalDate.of(2026, 9, 11), 2);
         StayBean stay = stays.get(0);
         List<AvailabilityBean> av = controller.findAvailability(stay);
         assertNotNull(av);
@@ -40,7 +40,7 @@ class BookStayControllerTest {
 
     @Test
     void testSendReservation() throws Exception {
-        List<StayBean> stays = controller.findStays("Rome", LocalDate.of(2025, 8, 5), LocalDate.of(2025, 8, 6), 2);
+        List<StayBean> stays = controller.findStays("Rome", LocalDate.of(2026, 9, 10), LocalDate.of(2026, 9, 11), 2);
         StayBean stay = stays.get(0);
         List<AvailabilityBean> av = controller.findAvailability(stay);
 
@@ -49,8 +49,8 @@ class BookStayControllerTest {
         booking.setLastName("User");
         booking.setEmailAddress("test.user@example.com");
         booking.setTelephone("+391234567890");
-        booking.setCheckInDate(LocalDate.of(2025, 8, 5));
-        booking.setCheckOutDate(LocalDate.of(2025, 8, 6));
+        booking.setCheckInDate(LocalDate.of(2026, 9, 10));
+        booking.setCheckOutDate(LocalDate.of(2026, 9, 11));
         booking.setNumGuests(2);
         booking.setOnlinePayment(false);
 
